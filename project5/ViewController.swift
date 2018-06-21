c2fb6c7afa1bd412c0d1d551ceb147fa230f3045
mohsina rahman <mohsinarahman@mohsinas-imac.local>
2018-06-21 01:41:58 -0700

    Adding button to the initial screen.

--- project5/ViewController.swift
+++ project5/ViewController.swift
@@ -8,18 +8,21 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var buttonGeneral: UIButton!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func buttonGeneralPressed(_ sender: Any)
+    {
+        
     }
-
+    
 
 }
 
